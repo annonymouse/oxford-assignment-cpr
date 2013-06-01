20d35b0f3cdde66538e60ca51b400dc14cde4495
"Alex Yong ext:(%22) <alexyong@cisco.com>
2013-06-01 19:36:53 +0100

    Finished part1, but get_bookmarks should match all tags

--- bookmark.erl
+++ bookmark.erl
@@ -19,15 +19,31 @@ remove(DB, Id) ->
     ets:delete(DB, Id),
     db_loop(DB).
 
+update_tags(DB, Id, UpdateTags) ->
+    case ets:lookup(DB, Id) of
+        [{Id, Url, Tags}] -> ets:insert(DB, {Id, Url, UpdateTags(Tags)}), ok;
+        [] -> {error, not_found}
+    end.
+
+tag(DB, Id, Tag, Dest) ->
+    Dest ! {addtagAck, update_tags(DB, Id, fun(Tags) -> [Tag|Tags] end)},
+    db_loop(DB).
+
+untag(DB, Id, Tag, Dest) ->
+    Dest ! {remtagAck, 
+        update_tags(DB, Id, fun(Tags) -> lists:delete(Tags,Tag) end)},
+    db_loop(DB).
+
 dump(DB, Dest, Fun) ->
+    % TODO Check ets:tab2list
     Res = ets:foldl(Fun, [], DB),
     Dest ! {got, Res},
     db_loop(DB).
 
 make_search(Match) ->
-    fun (Elem) ->
-            lists:any(fun(IElem) -> Elem == IElem end, Match) end.
+    fun (Elem) -> lists:any(fun(IElem) -> Elem == IElem end, Match) end.
 
+% TODO See ets:fun2ms
 make_matcher(Match) ->
     % We're doing a crap solution here, TODO make this faster by sorting 
     % list patterns.
@@ -42,6 +58,8 @@ db_loop(DB) ->
     receive
         {add, Url, Tags, Dest} -> add(DB, Url, Tags, Dest);
         {remove, Id} -> remove(DB, Id);
+        {addtag, Id, Tag, Dest} -> tag(DB, Id, Tag, Dest);
+        {remtag, Id, Tag, Dest} -> untag(DB, Id, Tag, Dest);
         {dump, Dest} -> dump(DB, Dest, 
                 fun({_Id, Bookmark,_Tags}, Acc) -> [Bookmark|Acc] end);
         {match, Match, Dest} -> dump(DB, Dest, make_matcher(Match)); 
@@ -68,9 +86,17 @@ add_bookmark(Url, Tags) ->
 remove_bookmark(Id) -> 
     bookmarks ! {remove, Id}, ok.
 
-add_tag(_Id, _Tag) -> {error, not_found}.
+add_tag(Id, Tag) -> 
+    bookmarks ! {addtag, Id, Tag, self()},
+    receive
+        {addtagAck, Res} -> Res 
+    end.
 
-remove_tag(_Id, _Tag) -> {error, not_implemented}.
+remove_tag(Id, Tag) -> 
+    bookmarks ! {remtag, Id, Tag, self()},
+    receive
+        {remtagAck, Res} -> Res
+    end.
 
 % get is a bif, so we can't use it as an atom here...
 get_bookmarks() -> 
